50a96481914912ed9690a84c709ba4969a27d939
J0rge08 <suarezinfantejorgemanuel@gmail.com>
2026-05-19 12:09:01 -0500

    Initialize models module with imports

---
+++ src/api/models/__init__.py
@@ -0,0 +1,6 @@
+"""Modelos de la base de datos."""
+from src.api.models.driver import Driver
+from src.api.models.alert_event import AlertEvent
+from src.api.models.driving_session import DrivingSession
+
+__all__ = ["Driver", "AlertEvent", "DrivingSession"]
